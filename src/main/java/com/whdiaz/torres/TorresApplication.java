fd90d25b14decb3963ec85894dd64724c5127cb2
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-30 21:00:06 -0300

    initial commit

---
+++ src/main/java/com/whdiaz/torres/TorresApplication.java
@@ -0,0 +1,13 @@
+package com.whdiaz.torres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whdiaz/torres/TorresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whdiaz.torres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
