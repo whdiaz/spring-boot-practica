3bc37c8c9f6eb6cde7dafa9a94da67fd2ad59b3f
willian honorio diaz torres <whdiaztorres@gmail.com>
2022-05-30 22:43:21 -0300

    implementando bootstrap

---
+++ src/main/java/com/whdiaz/torres/controllers/UsuarioController.java
@@ -0,0 +1,16 @@
+package com.whdiaz.torres.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+    public List<String> prueba(){
+        return List.of("willian", "samuel", "bernardo");
+    }
+
+}
